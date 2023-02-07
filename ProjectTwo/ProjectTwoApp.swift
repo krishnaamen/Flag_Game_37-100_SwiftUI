1c93b0d1fc8a53a97fc69f1bc7cbee03e36123c0
Krishna Prasad Khanal <krishnaamen@gmail.com>
2023-02-07 11:09:57 +0100

    Initial Commit

---
+++ ProjectTwo/ProjectTwoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectTwoApp.swift
+//  ProjectTwo
+//
+//  Created by min phone on 06/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct ProjectTwoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
